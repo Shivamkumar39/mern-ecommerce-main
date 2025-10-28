65bb35dbcdcf1acc788e0d8c9211617498efe2f8
Shivamkumar098798 <shivamkumar098798@gmail.com>
2025-10-28 14:18:02 +0530

    commit

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -4,24 +4,15 @@ const { sanitizeUser } = require('../utils/SanitizeUser')
 
 exports.verifyToken=async(req,res,next)=>{
     try {
-        // extract the token from request cookies
         const {token}=req.cookies
-
-        // if token is not there, return 401 response
         if(!token){
             return res.status(401).json({message:"Token missing, please login again"})
         }
-
-        // verifies the token 
         const decodedInfo=jwt.verify(token,process.env.SECRET_KEY)
-
-        // checks if decoded info contains legit details, then set that info in req.user and calls next
         if(decodedInfo && decodedInfo._id && decodedInfo.email){
             req.user=decodedInfo
             next()
         }
-
-        // if token is invalid then sends the response accordingly
         else{
             return res.status(401).json({message:"Invalid Token, please login again"})
         }
